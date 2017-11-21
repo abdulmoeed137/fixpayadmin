8b70f931a949c79264fd63eb11a310b24826febd
abdul moeed <abdulmoeedkhan92@gmail.com>
2017-11-21 16:17:32 +0500

    ok

---
+++ app/src/main/java/admin/money/fixshix/com/fixshixmoneyadmin/Constants.java
@@ -0,0 +1,18 @@
+package admin.money.fixshix.com.fixshixmoneyadmin;
+
+/**
+ * Created by lenovo on 8/26/2017.
+ */
+
+public class Constants {
+    public  static  String MAINURL = "http://fixpay.pk/fixshix_money_api/merchant_api/";
+    public  static  String login = MAINURL+"login.php";
+    public  static  String make_transaction_qr= MAINURL+"make_transaction_qr.php";
+    public  static  String make_transaction = MAINURL+"make_transaction.php";
+    public  static  String merchant_history = MAINURL+"merchant_history.php";
+
+
+
+    public  static  String SESSION="FixshixMoneyAdminFinal";
+    public  static  String QR_KEY_RECEIVE="2382784738r3847834392382989238";
+}
